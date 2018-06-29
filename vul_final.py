ac0080c9493a86a5bdcf1945517d516b21c0ae0b
MUSH7OOM <nks0318@naver.com>
2018-06-29 17:56:43 +0900

    add index
    index항목 추가, 항목 색 추가

--- vul_final.py
+++ vul_final.py
@@ -20,7 +20,8 @@ html_header = '''<!DOCTYPE html>
 <table id="visit-stat-table" class="table table-sorting table-striped table-hover datatable">
     <thead>
     <tr>
-        <th>Vunerability Index / Title</th>
+        <th width="20">Index</th>
+        <th>Vunerability Title</th>
         <th>Check Result</th>
      </tr>
     </thead>
@@ -55,9 +56,11 @@ title.close()
 check.close()
 title = open("title",'r')
 check = open("check",'r')
+index = open("index",'r')
 
 d_title = title.readlines()
 d_check = check.readlines()
+d_index = index.readlines()
 
 report = open("report.html",'w')
 os_version = open("osversion",'r')
@@ -72,33 +75,28 @@ for i in range(1):
 #list(range(22))
 
 for i in range(22):
+    if i == 0 :
+        report.write("<tr><td colspan='3' align='center' bgcolor='darksalmon'>")
+        report.write("1 Acouunts Management</td></tr>")
+
+    if i == 5 :
+        report.write("<tr><td colspan='3' align='center' bgcolor='aquamarine'>")
+        report.write("2 File & Directories Management</td></tr>")
+
+    if i == 15 :
+        report.write("<tr><td colspan='3' align='center' bgcolor='cornflowerblue'>")
+        report.write("3 Service Management </td></tr>")
+
     report.write("<tr>")
+    report.write("<td>"+d_index[i]+"</td>")
     report.write("<td>"+d_title[i]+"</td>")
     report.write("<td>"+d_check[i]+"</td>")
     report.write("</tr>")
-
-
 
 report.write(html_fotter)
 report.close()
 
 os.remove('check')
 os.remove('title')
+os.remove('index')
 os.remove('osversion')
-        #for rline in lines:
-        #    if "Check" in rline:
-        #        rline=rline.replace("Check Result : ","")
-        #        check.write(rline)
-
-
-        #for line in lines:
-        #	if "========" in line:
-        #		line = line.replace("========","")
-        #        title.write(line)
-        #        result.write("<tr>")
-        #        result.write("<td>"+title[0]+"<td>")
-        #        result.write("</tr>")
-
-        #result.write(html_fotter)
-        #f.close()
-        #resul.close()
